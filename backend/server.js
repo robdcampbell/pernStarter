74108da4f7921e0968dd2615f21f9a53d5a867e5
robdcampbell <rdeancampbell@gmail.com>
2021-07-01 23:55:20 -0400

    prepare for Hekou Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 5000;
+const PORT = process.env.port | 5000;
 const pool = require("./config/db");
 // const jwtAuth = require('./routes/jwtAuth.js')
 

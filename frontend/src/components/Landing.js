a60f5441fa564bd6fa490754a4a0017ef943f6f6
robdcampbell <rdeancampbell@gmail.com>
2021-12-20 16:45:15 -0500

    Additional notes and edited UI of Landing page

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <section className="registration">
-      <h1>Stater Pack.</h1>
-      <p>Sign in to get to your dashboard:</p>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+      <h1>p.e.r.n. Stater Pack.</h1>
+      <p>Sign in / Register to reach your dashboard:</p>
+      <div className="landing-controls">
+        <Link to="/login">Login Here</Link>
+        <Link to="/register">Register a new account here</Link>
+      </div>
     </section>
   );
 };
